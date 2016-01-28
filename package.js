cdc46395725f4bdb3aa386ddd4be1ac01098a37b
Abigail Watson <awatson1978@gmail.com>
2016-01-27 21:27:11 -0800

    only load theme on client

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:default-theme',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Default color theme for ClinicalFramework apps.',
   git: 'https://github.com/clinical-meteor/default-theme',
   documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('clinical:theming@0.4.5');
-  api.addFiles('default-theme.js');
+  api.addFiles('default-theme.js', 'client');
 });
 
 Package.onTest(function (api) {
